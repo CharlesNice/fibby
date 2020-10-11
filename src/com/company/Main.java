a9aca08ca8e18fc4abb379c2def369d503791692
C <charlienice123@gmail.com>
2020-10-11 20:04:26 +0100

    fib example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int fibUp2 = 1;
+
+        int[] fib = new int[fibUp2+2];
+        fib[0]=0;
+        fib[1]=1;
+
+        //System.out.println(fib[0]);
+        //System.out.println(fib[1]);
+        for(int i = 2; i<fibUp2+2; i++){
+            fib[i] = fib[i-1] + fib[i-2];
+            //System.out.println(fib[i]);
+        }
+        System.out.println(fib[fibUp2]);
     }
 }
